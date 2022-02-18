22ee74c4adeef759d9ed3dc1cd234ea85ca5512c
kawuma20 <98647264+kawuma20@users.noreply.github.com>
2022-02-18 14:20:25 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -6,3 +6,32 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 
+Future<void> main() async {
+  await Hive.initFlutter();
+  //**Registering the database */
+  Hive.registerAdapter(ItemsAdapter());
+
+  Hive.openBox(dbStore);
+
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.android,
+  );
+  runApp(const Client());
+}
+
+class Client extends StatelessWidget {
+  const Client({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Client App',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
+      home: const Intro(),
+    );
+  }
+}
